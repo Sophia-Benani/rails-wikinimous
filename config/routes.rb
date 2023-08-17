c5edff50c262f7916942fcde763bb48e75c3cedb
Sophia Benani <sophiabenani@gmail.com>
2023-08-17 10:18:01 +0200

    exercise done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new', as: :new_article
+  post 'articles', to: 'articles#create'
+  get 'articles/:id', to: 'articles#show', as: :article
 end
